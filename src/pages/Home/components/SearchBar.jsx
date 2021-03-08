870442055ce13874be2b9d2e7d58f26f7f51b7ae
petrgali <opelliek@gmail.com>
2021-03-08 13:17:10 +0600

    feat: add clear field button and some improvements

--- src/pages/Home/components/SearchBar.jsx
+++ src/pages/Home/components/SearchBar.jsx
@@ -1,6 +1,8 @@
 import { Container, IconButton, InputBase, makeStyles, Paper } from "@material-ui/core"
+import Divider from "@material-ui/core/Divider"
+import { useState } from "react"
 import SearchIcon from "@material-ui/icons/Search"
-// import TextField from "@material-ui/core/TextField"
+import CloseIcon from "@material-ui/icons/Close"
 
 
 const useStyles = makeStyles((theme) => ({
@@ -12,28 +14,43 @@ const useStyles = makeStyles((theme) => ({
     },
     input: {
         marginLeft: theme.spacing(1),
-        flex: 1
-    }
+        flex: 1,
+        fontSize: "large"
+    },
+    iconButton: {
+        padding: 10,
+    },
 }))
-
-const handleSearch = (event) => {
-    event.preventDefault()
-}
 
 export default function SearchBar() {
     const classes = useStyles()
+    let [searchMsg, updateMsg] = useState('')
+    /*Search logic placed here*/
+
+    const handleSearch = (event) => {
+        event.preventDefault()
+        alert(`SEARCH REQUEST FOR ${searchMsg}`)
+    }
     return (
         <Container maxWidth="md"
-            onChange={() => console.log("!")}
-            onClick={(event) => handleSearch(event)}>
-            <Paper component="form" className={classes.root} >
+            onSubmit={(event) => handleSearch(event)}>
+            <Paper component="form" elevation={3} className={classes.root} >
                 <InputBase
+                    value={searchMsg}
+                    onChange={(event) => updateMsg(event.target.value)}
                     autoFocus={true}
                     className={classes.input}
                     placeholder="search..."
+
                 />
-                <IconButton type="submit" aria-label="search">
+                <IconButton type="submit" aria-label="search" className={classes.iconButton}>
                     <SearchIcon />
+                </IconButton>
+                <Divider className={classes.divider} orientation="vertical" />
+                <IconButton aria-label="close"
+                    className={classes.iconButton}
+                    onClick={() => updateMsg('')}>
+                    <CloseIcon />
                 </IconButton>
             </Paper>
         </ Container>
