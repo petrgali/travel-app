6593fcdfa5ee2cdeb5d1be5db71efed57a8bfff9
Adilyam <adilyamt@gmail.com>
2021-03-11 22:37:57 +0600

    feat: add dynamic language change in header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,73 @@
 import React from "react"
 import { makeStyles } from "@material-ui/core/styles"
 import AppBar from "@material-ui/core/AppBar"
-import Toolbar from "@material-ui/core/Toolbar"
 import Button from "@material-ui/core/Button"
-import IconButton from "@material-ui/core/IconButton"
+import Menu from "@material-ui/core/Menu"
+import MenuItem from "@material-ui/core/MenuItem"
+
+import i18n from "../i18n"
+import { withNamespaces } from "react-i18next"
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        // flexGrow: 1,
         height: "3rem",
         justifyContent: "center",
         alignItems: "flex-end",
-        paddingRight: "1rem",
     },
     menuButton: {
         marginRight: theme.spacing(2),
-    },
-    title: {
-        flexGrow: 1,
+        color: "white",
     },
 }))
 
-const Header = () => {
+const Header = ({ t }) => {
+    const [anchorEl, setAnchorEl] = React.useState(null)
+
+    const languages = ["en", "ru"]
+    const changeLanguage = (lng) => {
+        i18n.changeLanguage(lng)
+    }
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget)
+    }
+
+    const handleClose = (e) => {
+        const { myValue } = e.currentTarget.dataset
+        changeLanguage(myValue)
+        setAnchorEl(null)
+    }
+
     const classes = useStyles()
     return (
         <AppBar position="fixed" className={classes.root}>
-            <Button color="inherit">en</Button>
+            <Button
+                aria-controls="simple-menu"
+                aria-haspopup="true"
+                onClick={handleClick}
+                className={classes.menuButton}
+            >
+                {i18n.language}
+            </Button>
+            <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+            >
+                {languages.map((lang, idx) => (
+                    <MenuItem
+                        key={idx}
+                        data-my-value={lang}
+                        onClick={handleClose}
+                    >
+                        {lang}
+                    </MenuItem>
+                ))}
+            </Menu>
         </AppBar>
     )
 }
 
-export default Header
+export default withNamespaces()(Header)
