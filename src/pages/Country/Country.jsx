a9ab1f806a00736ea34ae3cd1968158a2c5afd03
Adilyam <adilyamt@gmail.com>
2021-03-15 17:22:34 +0600

    refactor: Country -> CountryDetail

--- src/pages/Country/Country.jsx
+++ src/pages/Country/Country.jsx
@@ -1,43 +1,12 @@
 import React, { useEffect } from "react"
-
-import { makeStyles } from "@material-ui/core/styles"
-import Card from "@material-ui/core/Card"
-import CardContent from "@material-ui/core/CardContent"
-import CardMedia from "@material-ui/core/CardMedia"
-
+import CountryDetail from "./components/CountryDetail"
 import { useSelector, useDispatch } from "react-redux"
 import { updateCountryDetail } from "../../redux/actions/countryDetailActions"
 
 import getCountryDetail from "../../services/getCountryDetail"
 
-const useStyles = makeStyles({
-    root: {
-        display: "flex",
-        height: "fit-content",
-        ["@media (max-width:780px)"]: {
-            // eslint-disable-line no-useless-computed-key
-            flexDirection: "column",
-        },
-    },
-    main: {
-        padding: "3rem",
-    },
-    details: {
-        display: "flex",
-        flexDirection: "column",
-    },
-    content: {
-        flex: "1 0 auto",
-    },
-    cover: {
-        minWidth: "30%",
-        minHeight: "10rem",
-    },
-})
-
 export function Country(props) {
     const id = props.match.params.id
-    const classes = useStyles()
 
     const dispatch = useDispatch()
     const country = useSelector((state) => state.countryDetail.countryDetail)
@@ -50,23 +19,7 @@ export function Country(props) {
         _getCountryDetail()
     }, [dispatch, id])
 
-    return (
-        <div className={classes.main}>
-            <Card className={classes.root}>
-                <CardMedia
-                    className={classes.cover}
-                    image={country.imageUrl}
-                    title={country.name}
-                />
-                <div className={classes.details}>
-                    <CardContent className={classes.content}>
-                        <h2>{country.name}</h2>
-                        <p>{country.description}</p>
-                    </CardContent>
-                </div>
-            </Card>
-        </div>
-    )
+    return <CountryDetail country={country}></CountryDetail>
 }
 
 export default Country
---
+++ src/pages/Country/components/CountryDetail.jsx
@@ -0,0 +1,54 @@
+import React from "react"
+
+import { makeStyles } from "@material-ui/core/styles"
+import Card from "@material-ui/core/Card"
+import CardContent from "@material-ui/core/CardContent"
+import CardMedia from "@material-ui/core/CardMedia"
+
+const useStyles = makeStyles({
+    root: {
+        display: "flex",
+        height: "fit-content",
+        // eslint-disable-next-line
+        ["@media (max-width:780px)"]: {
+            flexDirection: "column",
+        },
+    },
+    main: {
+        padding: "3rem",
+    },
+    details: {
+        display: "flex",
+        flexDirection: "column",
+    },
+    content: {
+        flex: "1 0 auto",
+    },
+    cover: {
+        minWidth: "30%",
+        minHeight: "10rem",
+    },
+})
+
+export const CountryDetail = ({ country }) => {
+    const classes = useStyles()
+
+    return (
+        <div className={classes.main}>
+            <Card className={classes.root}>
+                <CardMedia
+                    className={classes.cover}
+                    image={country.imageUrl}
+                    title={country.name}
+                />
+                <div className={classes.details}>
+                    <CardContent className={classes.content}>
+                        <h2>{country.name}</h2>
+                        <p>{country.description}</p>
+                    </CardContent>
+                </div>
+            </Card>
+        </div>
+    )
+}
+export default CountryDetail
