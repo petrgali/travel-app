c0029ac09b54827bee5eeba5d55bfc65bd6dee60
Adilyam <adilyamt@gmail.com>
2021-03-15 15:40:52 +0600

    refactor: Home.jsx, countrycard

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,29 @@
+import { useEffect } from "react"
 import CountryCard from "./components/CountryCard"
+import { useSelector, useDispatch } from "react-redux"
+import { updateCountries } from "../../redux/actions/countryActions"
+
+import getCountries from "../../services/getCountries"
 
 export default function Home() {
+    const dispatch = useDispatch()
+    const countries = useSelector((state) => state.country.countries)
+
+    useEffect(() => {
+        const _getCountries = async () => {
+            const res = await getCountries()
+            if (res) dispatch(updateCountries(res))
+        }
+        _getCountries()
+    }, [dispatch])
+
+    const handleClick = () => {
+        console.log("Clicked")
+    }
+
     return (
         <>
-            <CountryCard />
+            <CountryCard countries={countries} handleClick={handleClick} />
         </>
     )
 }
